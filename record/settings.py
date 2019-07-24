7b90464d3b9bb9e063c2afa67dbb0bb0523bec86
JuYeong0413 <2jy22@naver.com>
2019-07-24 23:15:54 +0900

    base에 템플릿 적용

--- record/settings.py
+++ record/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'record', 'templates')
+    os.path.join(BASE_DIR, 'record', 'static')
 ]
 
 MEDIA_URL = '/media/'
