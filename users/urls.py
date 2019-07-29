bceb36ac3759970ede225c10692bd8a941c993a0
JuYeong0413 <2jy22@naver.com>
2019-07-29 20:10:46 +0900

    users views

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 app_name = "users"
 urlpatterns = [
+    path('<int:id>/', views.main, name="main"),
+    path('edit/<int:id>/', views.edit, name="edit"),
+    path('update/<int:id>/', views.update, name="update"),
     path('follow_toggle/<int:id>/', views.follow_toggle, name="follow_toggle"),
+    path('<int:id>/musics/', views.musics, name="musics"),
+    path('<int:id>/playlists/', views.playlists, name="playlists"),
+    path('<int:id>/likes/', views.likes, name="likes"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,43 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from musics.models import Music
+from playlists.models import Playlist
 from .models import User
 
+# Create your views here.
+# 프로필 페이지
+def main(request, id):
+    user_profile = get_object_or_404(User, pk=id)
+    return render(request, 'users/main.html', {'user_profile': user_profile})
+
+
+# 프로필 수정 페이지
+def edit(request, id):
+    current_user = request.user
+    user = get_object_or_404(User, pk=id)
+
+    if user == current_user:
+        return render(request, 'users/edit.html', {'user': user})
+    else:
+        return redirect('main')
+
+
+# 프로필 수정
+def update(request, id):
+    if request.method == "POST":
+        user = get_object_or_404(User, pk=id)
+        username = request.POST.get('username')
+        introduction = request.POST.get('introduction')
+        user.username = username
+        user.introduction = introduction
+
+        if request.FILES.get('image'):
+            user.image = request.FILES.get('image')
+
+        user.save()
+        return redirect('users:main', id)
+
+
+# 팔로우, 언팔로우
 def follow_toggle(request, id):
     user = request.user
     if user.is_anonymous:
@@ -16,3 +53,22 @@ def follow_toggle(request, id):
         user.followings.add(followed_user)
 
     return redirect('playlists:main')
+
+
+# 작성한 노래 게시글
+def musics(request, id):
+    musics = Music.objects.filter(writer__id=id)
+    return render(request, 'users/musics.html', {'musics': musics})
+
+
+# 생성한 플레이리스트 게시글
+def playlists(request, id):
+    playlists = Playlist.objects.filter(creator__id=id)
+    return render(request, 'users/playlists.html', {'playlists': playlists})
+
+
+# 좋아하는 플레이리스트 목록
+def likes(request, id):
+    user = get_object_or_404(User, pk=id)
+    playlists = Playlist.objects.filter(likes = user)
+    return render(request, 'users/likes.html', {'playlists': playlists})
