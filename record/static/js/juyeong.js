93b6b91da7219b6567f8c7e156c363f47c48a8c0
JuYeong0413 <2jy22@naver.com>
2019-08-05 18:38:59 +0900

    가사 펼치기/접기

---
+++ record/static/js/juyeong.js
@@ -0,0 +1,13 @@
+$('#show_lyrics').click(function() {
+    $('#hide_lyrics').removeClass('hide');
+    $('#show_lyrics').addClass('hide');
+
+    $('#lyrics').removeClass('hide');
+});
+
+$('#hide_lyrics').click(function() {
+    $('#show_lyrics').removeClass('hide');
+    $('#hide_lyrics').addClass('hide');
+
+    $('#lyrics').addClass('hide');
+});
