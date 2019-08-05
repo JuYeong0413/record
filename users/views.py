27d867596eb6885f679cb2bad399874c1fc58182
JuYeong0413 <2jy22@naver.com>
2019-08-05 18:09:45 +0900

    user edit 수정

--- users/views.py
+++ users/views.py
@@ -18,7 +18,7 @@ def edit(request, id):
     if user == current_user:
         return render(request, 'users/edit.html', {'user': user})
     else:
-        return redirect('main', id)
+        return redirect('users:main', id)
 
 
 # 프로필 수정
