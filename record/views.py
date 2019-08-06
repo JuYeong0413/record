efe4369ba83dc80c8f53819421e91e68814c973c
Leejijijiji <jhlee981107@naver.com>
2019-08-06 21:47:17 +0900

    바뀐거 없어요

--- record/views.py
+++ record/views.py
@@ -4,5 +4,5 @@ from playlists.models import Playlist
 # Create your views here.
 def main(request):
 	playlists = Playlist.objects.all()[:3]
-
+	
 	return render(request, 'main.html', {'playlists': playlists})
