0da8cd76f90e162edd92de251bea6c78c8acc7c3
JuYeong0413 <2jy22@naver.com>
2019-08-08 15:30:31 +0900

    add option

--- musics/views.py
+++ musics/views.py
@@ -56,6 +56,11 @@ def create(request):
         options.add_argument('headless')
         options.add_argument('window-size=1920x1080')
         options.add_argument('--disable-gpu')
+        options.add_argument("start-maximized")
+        options.add_argument("disable-infobars")
+        options.add_argument("--disable-extensions")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
 
         driver = webdriver.Chrome(executable_path='./chromedriver', chrome_options=options)
         driver.get('https://www.melon.com/search/song/index.htm?q={}+{}&section=&searchGnbYn=Y&kkoSpl=Y&kkoDpType=&linkOrText=T&ipath=srch_form'.format(singer, title))
